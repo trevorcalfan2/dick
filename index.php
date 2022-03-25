c00b2b13cb141fc210b9ff55c1968bf7ebfcb29e
trevorcalfan2 <51901588+trevorcalfan2@users.noreply.github.com>
2022-03-25 15:54:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Conexion a la bd</title>
+</head>
+<body>
+    <?php
+
+        echo("php en la nube");
+        
+    ?>
+
+</body>
+</html>
+
+    
+
